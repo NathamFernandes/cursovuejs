f7cc4f84edc2a0bae0a0bf908dfd29850bf29e3f
NathamFernandes <natham.fr@gmail.com>
2023-11-12 12:30:07 -0300

    Partial L8

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            onSale: true,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 100, onSale: true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
             ]
         }
     },
@@ -17,8 +17,24 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+            this.onSale = this.variants[this.selectedVariant].onSale 
+        }
+    },
+    computed: {
+        title() {
+            return this.product + ' ' + this.brand
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        showOnSale() {
+            return this.brand + ' ' + this.product + ' is on sale'
         }
     }
+
 })
