f1b0df3a6f00a11b441e9e8a4566050c9a1169a5
NathamFernandes <natham.fr@gmail.com>
2023-11-12 10:43:33 -0300

    L4 lession

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            produto: 'Meias da Vue',
+            descricao: 'Meias de algodão e poliéster da Vue. Obrigado pelo curso.',
+            url: "https://natham.vercel.app/",
+            imagem: './assets/images/socks_blue.jpg',
+            quantidade: 100,
+            estaAVenda: true
         }
     }
 })
